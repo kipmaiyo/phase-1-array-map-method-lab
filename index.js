c485dbd3ebc0e592db58c00df7306af15a17218a
kipmaiyo <danielkipmaiyo44@gmail.com>
2023-07-07 20:06:44 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +12,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased() {
+  return tutorials.map(function(tutorial) {
+    return tutorial.split(' ').map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(' ')
+  })
 }
+console.log(titleCased())
